348ae6c1001bc880f7f551c1fdb588618cc40c84
MattiasSp <mattias.spangmyr@gmail.com>
2021-06-23 14:30:39 +0200

    First published version

---
+++ tasks/create_windowsservice.js
@@ -0,0 +1,16 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'gcs2postgresql',
+  description: 'NodeJS server app for fetching JSON from a Google Cloud Storage API and inserting it into PostgreSQL with repeated intervals.',
+  script: '..\\gcs2postgresql.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
---
+++ tasks/uninstall_windowsservice.js
@@ -0,0 +1,17 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'gcs2postgresql',
+  description: 'NodeJS server app for fetching JSON from a Google Cloud Storage API and inserting it into PostgreSQL with repeated intervals.',
+  script: '..\\gcs2postgresql.js'
+});
+
+// Listen for the "uninstall" event
+svc.on('uninstall',function(){
+  console.log('Uninstall complete.');
+  console.log('The service exists: ',svc.exists);
+});
+
+// Uninstall the service.
+svc.uninstall();
