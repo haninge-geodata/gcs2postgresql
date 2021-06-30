ca835857348a61523a83127ce9257acfc0d80f1c
MattiasSp <mattias.spangmyr@gmail.com>
2021-06-30 15:34:35 +0200

    Dependecy and linter fixes

--- tasks/create_windowsservice.js
+++ tasks/create_windowsservice.js
@@ -1,15 +1,15 @@
-var Service = require('node-windows').Service;
+import { Service } from 'node-windows';
 
 // Create a new service object
-var svc = new Service({
-  name:'gcs2postgresql',
+const svc = new Service({
+  name: 'gcs2postgresql',
   description: 'NodeJS server app for fetching JSON from a Google Cloud Storage API and inserting it into PostgreSQL with repeated intervals.',
   script: '..\\gcs2postgresql.js'
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
+svc.on('install', () => {
   svc.start();
 });
 
